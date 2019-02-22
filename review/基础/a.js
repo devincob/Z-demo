64d39d93a0b1a270bf5a56cb80e372979597e14a
qr <devincob@qq.com>
2019-02-22 16:43:18 +0800

    ...AMD

---
+++ review/基础/a.js
@@ -0,0 +1,11 @@
+/**
+ * Created by devin on 2019/2/20.
+ */
+define(['./a-util.js'], function(aUtil){
+    var a ={
+        printDate:function (date) {
+            console.log(aUtil.aGetFormatDate(date))
+        }
+    }
+    return a
+})
---
+++ review/基础/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by devin on 2019/2/20.
+ */
+define(['./a.js'], function(a){
+    var date =new Date()
+    a.printDate(date)
+})
