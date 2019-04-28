185535fa779cb2d5f96369cef70b2ad58ec27955
qr <devincob@qq.com>
2019-04-28 16:38:13 +0800

    ...

--- python/面向对象编程/实例属性和类属性.py
+++ python/面向对象编程/实例属性和类属性.py
@@ -1,21 +1,77 @@
-#-*- coding:utf-8 -*-
-#Authon: devincob
+# -*- coding:utf-8 -*-
+# Authon: devincob
 
-# 面向对象最重要的概念就是类（Class）和实例（Instance），
+# 实例属性和类属性
 
-# 必须牢记类是抽象的模板，比如Student类，而实例是根据类创建出来的一个个具体的“对象”，
+# 由于Python是动态语言,根据类创建的实例可以任意绑定属性。
 
-# 每个对象都拥有相同的方法，但各自的数据可能不同。
-
-# 仍以Student类为例，在Python中，定义类是通过class关键字：
+# 给实例绑定属性的方法是通过实例变量,或者通过self变量：
 
 class Student(object):
-    pass
+    def __init__(self, name):
+        self.name = name
 
-# class后面紧接着是类名，即Student，类名通常是大写开头的单词，
 
-# 紧接着是(object)，表示该类是从哪个类继承下来的，继承的概念我们后面再讲，
+s = Student('Bob')
+s.score = 90
 
-# 通常，如果没有合适的继承类，就使用object类，这是所有类最终都会继承的类。
 
-# 定义好了Student类，就可以根据Student类创建出Student的实例，创建实例是通过类名+()实现的：
+# 但是,如果Student类本身需要绑定一个属性呢？可以直接在class中定义属性,这种属性是类属性,归Student类所有：
+
+class Student(object):
+    name = 'Student'
+
+
+# 当我们定义了一个类属性后,这个属性虽然归类所有,但类的所有实例都可以访问到。来测试一下：
+
+
+class Student(object):
+    name = 'Student'
+
+s = Student()  # 创建实例s
+print('Student',s.name)  # 打印name属性,因为实例并没有name属性,所以会继续查找class的name属性
+# Student
+print('Student',Student.name)  # 打印类的name属性
+# Student
+s.name = 'Michael'  # 给实例绑定name属性
+print(s.name)  # 由于实例属性优先级比类属性高,因此,它会屏蔽掉类的name属性
+# Michael
+print(Student.name)  # 但是类属性并未消失,用Student.name仍然可以访问
+# Student
+del s.name  # 如果删除实例的name属性
+print(s.name)  # 再次调用s.name,由于实例的name属性没有找到,类的name属性就显示出来了
+Student
+# 从上面的例子可以看出, 在编写程序的时候, 千万不要对实例属性和类属性使用相同的名字,
+#  因为相同名称的实例属性将屏蔽掉类属性, 但是当你删除实例属性后, 再使用相同的名称, 访问到的将是类属性。
+
+
+# 为了统计学生人数, 可以给Student类增加一个类属性, 每创建一个实例, 该属性自动增加：
+
+# -*- coding: utf-8 -*-
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
+# 小结
+# 实例属性属于各个实例所有, 互不干扰；
+#
+# 类属性属于类所有, 所有实例共享一个属性；
+#
+# 不要对实例属性和类属性使用相同的名字, 否则将产生难以发现的错误。
