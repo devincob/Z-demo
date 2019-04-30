7c700d0d0e19584dee7845cc3a78dd9eb72a987f
qr <devincob@qq.com>
2019-04-30 15:49:26 +0800

    ...

--- python/函数/函数的参数.py
+++ python/函数/函数的参数.py
@@ -6,21 +6,19 @@
 # Python的函数定义非常简单，但灵活度却非常大。除了正常定义的必选参数外，还可以使用默认参数、
 # 可变参数和关键字参数，使得函数定义出来的接口，不但能处理复杂的参数，还可以简化调用者的代码。
 #
-# 位置参数
-# 我们先写一个计算x2的函数：
+# 位置参数     我们先写一个计算x2的函数：
 
 def power(x):
     return x * x
 
-# 对于power(x)函数，参数x就是一个位置参数。
-# 当我们调用power函数时，必须传入有且仅有的一个参数x：
+# 对于power(x)函数，参数x就是一个位置参数。当我们调用power函数时，必须传入有且仅有的一个参数x：
 
 five = power(5)
-print(five)
+print('位置参数',five)
 fifteen = power(15)
-print(fifteen)
-# 现在，如果我们要计算x3怎么办？可以再定义一个power3函数，但是如果要计算x4、x5……怎么办？我们不可能定义无限多个函数。
-#
+print('位置参数',fifteen)
+# 现在，如果我们要计算x3怎么办？可以再定义一个power3函数，但是如果要计算x4、x5……怎么办？
+
 # 你也许想到了，可以把power(x)修改为power(x, n)，用来计算xn，说干就干：
 
 def power(x, n):
@@ -29,12 +27,13 @@ def power(x, n):
         n = n - 1
         s = s * x
     return s
+
 # 对于这个修改后的power(x, n)函数，可以计算任意n次方：
 
 five_square = power(5, 2)
 
 five_cube = power(5, 3)
-print(five_cube)
+print('位置参数',five_cube)
 # 修改后的power(x, n)函数有两个参数：x和n，这两个参数都是位置参数，调用函数时，传入的两个值按照位置顺序依次赋给参数x和n。
 
 # 默认参数
@@ -77,9 +76,9 @@ def enroll(name, gender):
     print('gender:', gender)
 # 这样，调用enroll()函数只需要传入两个参数：
 
-enroll('Sarah', 'F')
-name: Sarah
-gender: F
+print(enroll('Sarah', 'F'))
+# name: Sarah
+# gender: F
 # 如果要继续传入年龄、城市等信息怎么办？这样会使得调用函数的复杂度大大增加。
 #
 # 我们可以把年龄和城市设为默认参数：
@@ -91,20 +90,22 @@ def enroll(name, gender, age=6, city='Beijing'):
     print('city:', city)
 # 这样，大多数学生注册时不需要提供年龄和城市，只提供必须的两个参数：
 
-enroll('Sarah', 'F')
-name: Sarah
-gender: F
-age: 6
-city: Beijing
+print(enroll('Sarah', 'F'))
+
+# name: Sarah
+# gender: F
+# age: 6
+# city: Beijing
 # 只有与默认参数不符的学生才需要提供额外的信息：
 
 enroll('Bob', 'M', 7)
-enroll('Adam', 'M', city='Tianjin')
+print(enroll('Adam', 'M', city='Tianjin'))
 # 可见，默认参数降低了函数调用的难度，而一旦需要更复杂的调用时，又可以传递更多的参数来实现。无论是简单调用还是复杂调用，函数只需要定义一个。
 #
 # 有多个默认参数时，调用的时候，既可以按顺序提供默认参数，比如调用enroll('Bob', 'M', 7)，意思是，除了name，gender这两个参数外，最后1个参数应用在参数age上，city参数由于没有提供，仍然使用默认值。
 #
-# 也可以不按顺序提供部分默认参数。当不按顺序提供部分默认参数时，需要把参数名写上。比如调用enroll('Adam', 'M', city='Tianjin')，意思是，city参数用传进去的值，其他默认参数继续使用默认值。
+# 也可以不按顺序提供部分默认参数。当不按顺序提供部分默认参数时，需要把参数名写上。比如调用enroll('Adam', 'M', city='Tianjin')，
+# 意思是，city参数用传进去的值，其他默认参数继续使用默认值。
 #
 # 默认参数很有用，但使用不当，也会掉坑里。默认参数有个最大的坑，演示如下：
 #
@@ -115,25 +116,26 @@ def add_end(L=[]):
     return L
 # 当你正常调用时，结果似乎不错：
 
-add_end([1, 2, 3])
-[1, 2, 3, 'END']
-add_end(['x', 'y', 'z'])
-['x', 'y', 'z', 'END']
+M = add_end(['x', 'y', 'z'])
+print(M)
+# ['x', 'y', 'z', 'END']
 # 当你使用默认参数调用时，一开始结果也是对的：
 
 add_end()
-['END']
+# ['END']
 # 但是，再次调用add_end()时，结果就不对了：
 
 add_end()
-['END', 'END']
+# ['END', 'END']
 add_end()
-['END', 'END', 'END']
+# ['END', 'END', 'END']
 # 很多初学者很疑惑，默认参数是[]，但是函数似乎每次都“记住了”上次添加了'END'后的list。
 
 # 原因解释如下：
 #
-# Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+# Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，
+#
+# 因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
 #
 #  定义默认参数要牢记一点：默认参数必须指向不变对象！
 # 要修改上面的例子，我们可以用None这个不变对象来实现：
@@ -146,9 +148,9 @@ def add_end(L=None):
 # 现在，无论调用多少次，都不会有问题：
 
 add_end()
-['END']
+# ['END']
 add_end()
-['END']
+# ['END']
 # 为什么要设计str、None这样的不变对象呢？因为不变对象一旦创建，对象内部的数据就不能修改，这样就减少了由于修改数据导致的错误。此外，由于对象不变，多任务环境下同时读取对象不需要加锁，同时读一点问题都没有。我们在编写程序时，如果可以设计一个不变对象，那就尽量设计成不变对象。
 #
 # 可变参数
@@ -166,15 +168,15 @@ def calc(numbers):
 # 但是调用的时候，需要先组装出一个list或tuple：
 
 calc([1, 2, 3])
-14
+# 14
 calc((1, 3, 5, 7))
-84
+# 84
 # 如果利用可变参数，调用函数的方式可以简化成这样：
 
-calc(1, 2, 3)
-14
-calc(1, 3, 5, 7)
-84
+# calc(1, 2, 3)
+# # 14
+# calc(1, 3, 5, 7)
+# 84
 # 所以，我们把函数的参数改为可变参数：
 
 def calc(*numbers):
@@ -186,19 +188,19 @@ def calc(*numbers):
 # 参数numbers接收到的是一个tuple，因此，函数代码完全不变。但是，调用该函数时，可以传入任意个参数，包括0个参数：
 
 calc(1, 2)
-5
+# 5
 calc()
-0
+# 0
 # 如果已经有一个list或者tuple，要调用一个可变参数怎么办？可以这样做：
 
 nums = [1, 2, 3]
 calc(nums[0], nums[1], nums[2])
-14
+# 14
 # 这种写法当然是可行的，问题是太繁琐，所以Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去：
 
 nums = [1, 2, 3]
 calc(*nums)
-14
+# 14
 # *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
 
 # 可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict。请看示例：
@@ -208,25 +210,25 @@ def person(name, age, **kw):
 # 函数person除了必选参数name和age外，还接受关键字参数kw。在调用该函数时，可以只传入必选参数：
 
 person('Michael', 30)
-name: Michael age: 30 other: {}
+# name: Michael age: 30 other: {}
 # 也可以传入任意个数的关键字参数：
 
 person('Bob', 35, city='Beijing')
-name: Bob age: 35 other: {'city': 'Beijing'}
+# name: Bob age: 35 other: {'city': 'Beijing'}
 person('Adam', 45, gender='M', job='Engineer')
-name: Adam age: 45 other: {'gender': 'M', 'job': 'Engineer'}
+# name: Adam age: 45 other: {'gender': 'M', 'job': 'Engineer'}
 # 关键字参数有什么用？它可以扩展函数的功能。比如，在person函数里，我们保证能接收到name和age这两个参数，但是，如果调用者愿意提供更多的参数，我们也能收到。试想你正在做一个用户注册的功能，除了用户名和年龄是必填项外，其他都是可选项，利用关键字参数来定义这个函数就能满足注册的需求。
 #
 # 和可变参数类似，也可以先组装出一个dict，然后，把该dict转换为关键字参数传进去：
 
 extra = {'city': 'Beijing', 'job': 'Engineer'}
 person('Jack', 24, city=extra['city'], job=extra['job'])
-name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
-当然，上面复杂的调用可以用简化的写法：
+# name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+# 当然，上面复杂的调用可以用简化的写法：
 
 extra = {'city': 'Beijing', 'job': 'Engineer'}
 person('Jack', 24, **extra)
-name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+# name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
 # **extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
 #
 # 命名关键字参数
@@ -261,7 +263,7 @@ def person(name, age, *args, city, job):
     print(name, age, args, city, job)
 # 命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
 
-person('Jack', 24, 'Beijing', 'Engineer')
+# person('Jack', 24, 'Beijing', 'Engineer')
 # Traceback (most recent call last):
 #   File "<stdin>", line 1, in <module>
 # TypeError: person() takes 2 positional arguments but 4 were given
@@ -274,7 +276,7 @@ def person(name, age, *, city='Beijing', job):
 # 由于命名关键字参数city具有默认值，调用时，可不传入city参数：
 
 person('Jack', 24, job='Engineer')
-Jack 24 Beijing Engineer
+# Jack 24 Beijing Engineer
 # 使用命名关键字参数时，要特别注意，如果没有可变参数，就必须加一个*作为特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数：
 
 def person(name, age, city, job):
@@ -293,25 +295,25 @@ def f2(a, b, c=0, *, d, **kw):
 # 在函数调用的时候，Python解释器自动按照参数位置和参数名把对应的参数传进去。
 
 f1(1, 2)
-a = 1 b = 2 c = 0 args = () kw = {}
+# a = 1 b = 2 c = 0 args = () kw = {}
 f1(1, 2, c=3)
-a = 1 b = 2 c = 3 args = () kw = {}
+# a = 1 b = 2 c = 3 args = () kw = {}
 f1(1, 2, 3, 'a', 'b')
-a = 1 b = 2 c = 3 args = ('a', 'b') kw = {}
+# a = 1 b = 2 c = 3 args = ('a', 'b') kw = {}
 f1(1, 2, 3, 'a', 'b', x=99)
-a = 1 b = 2 c = 3 args = ('a', 'b') kw = {'x': 99}
+# a = 1 b = 2 c = 3 args = ('a', 'b') kw = {'x': 99}
 f2(1, 2, d=99, ext=None)
-a = 1 b = 2 c = 0 d = 99 kw = {'ext': None}
+# a = 1 b = 2 c = 0 d = 99 kw = {'ext': None}
 # 最神奇的是通过一个tuple和dict，你也可以调用上述函数：
 
 args = (1, 2, 3, 4)
 kw = {'d': 99, 'x': '#'}
 f1(*args, **kw)
-a = 1 b = 2 c = 3 args = (4,) kw = {'d': 99, 'x': '#'}
+# a = 1 b = 2 c = 3 args = (4,) kw = {'d': 99, 'x': '#'}
 args = (1, 2, 3)
-kw = {'d': 88, 'x': '#'}
+# kw = {'d': 88, 'x': '#'}
 f2(*args, **kw)
-a = 1 b = 2 c = 3 d = 88 kw = {'x': '#'}
+# a = 1 b = 2 c = 3 d = 88 kw = {'x': '#'}
 # 所以，对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的。
 #
 #  虽然可以组合多达5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差。
@@ -323,24 +325,24 @@ def product(x, y):
     return x * y
 
 # 测试
-print('product(5) =', product(5))
+# print('product(5) =', product(5))
 print('product(5, 6) =', product(5, 6))
-print('product(5, 6, 7) =', product(5, 6, 7))
-print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
-if product(5) != 5:
-    print('测试失败!')
-elif product(5, 6) != 30:
-    print('测试失败!')
-elif product(5, 6, 7) != 210:
-    print('测试失败!')
-elif product(5, 6, 7, 9) != 1890:
-    print('测试失败!')
-else:
-    try:
-        product()
-        print('测试失败!')
-    except TypeError:
-        print('测试成功!')
+# print('product(5, 6, 7) =', product(5, 6, 7))
+# print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+# if product(5) != 5:
+#     print('测试失败!')
+# elif product(5, 6) != 30:
+#     print('测试失败!')
+# elif product(5, 6, 7) != 210:
+#     print('测试失败!')
+# elif product(5, 6, 7, 9) != 1890:
+#     print('测试失败!')
+# else:
+#     try:
+#         product()
+#         print('测试失败!')
+#     except TypeError:
+#         print('测试成功!')
 # 小结
 # Python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
 #
--- python/高级特性/列表生成式.py
+++ python/高级特性/列表生成式.py
@@ -1,2 +1,38 @@
 #-*- coding:utf-8 -*-
 #Authon: devincob
+
+# 列表生成式即List Comprehensions，是Python内置的非常简单却强大的可以用来创建list的生成式。
+
+# 举个例子，要生成list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]可以用list(range(1, 11))：
+
+list = list(range(1,11))
+print(list)
+
+# 但如果要生成[1x1, 2x2, 3x3, ..., 10x10]怎么做？方法一是循环：
+
+l = []
+
+for x in range(1,11):
+    l.append(x*x)
+print(l)
+
+# 但是循环太繁琐，而列表生成式则可以用一行语句代替循环生成上面的list：
+
+lx = [x*x for x in range(1,11)]
+print(lx)
+
+# 写列表生成式时，把要生成的元素x * x放到前面，后面跟for循环，
+# 就可以把list创建出来，十分有用，多写几次，很快就可以熟悉这种语法。
+# for循环后面还可以加上if判断，这样我们就可以筛选出仅偶数的平方：
+
+lc = [x*x for x in range(1,11) if x%2 == 0]
+print(lc)
+# 还可以使用两层循环，可以生成全排列：
+string = [m + n for m in 'ABC' for n in 'XYZ']
+print(string)
+# ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+
+double = [m * n for m in [1,2,3] for n in [4,5,6]]
+
+print(double)
+
